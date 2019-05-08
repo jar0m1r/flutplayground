42431e84cf6c83e52dfccee3da3562569a49636e
Jaromir Humbitious <jaromir.vangorsel@gmail.com>
2019-05-08 21:58:43 +0200

    Add Product Create Page Fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,23 +1,64 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('SAVE'),
-        onPressed: (){
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context){
-              return Center(
-                child: Text('This is a Modal!')
-              );
-            }
-          )
-        }
-      )
+    return Column(
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Product Title',
+          ),
+          onChanged: (String value){ 
+            setState((){ 
+              titleValue = value;
+            });
+          }
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value){ 
+            setState((){ 
+              descriptionValue = value;
+            });
+          }
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value){ 
+            setState((){ 
+              priceValue = double.parse(value);
+            });
+          }
+        ),
+      ]
     );
   }
 }
+
+// return Center(
+//       child: RaisedButton(
+//         child: Text('SAVE'),
+//         onPressed: (){
+//           showModalBottomSheet(
+//             context: context,
+//             builder: (BuildContext context){
+//               return Center(
+//                 child: Text('This is a Modal!')
+//               );
+//             }
+//           )
+//         }
+//       )
+//     );
